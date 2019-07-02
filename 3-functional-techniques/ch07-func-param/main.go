6152b10e833faf8c30b05f85f0e20c8fbfd24a76
JayJay <youngtip@gmail.com>
2019-07-02 10:22:04 +0900

    .

---
+++ 3-functional-techniques/ch07-func-param/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func init() {
+	GetOptions()
+	InitLog("trace-log.txt", ioutil.Discard, os.Stdout, os.Stderr)
+}
+
+func main() {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	Info.Printf("Config %+v", Config)
+
+	newServer, err := server.New(
+		server.MaxConcurrentConnections(4),
+		server.MaxNumber(256), // Config.MaxNumber
+		server.UseNumberHandler(true),
+		server.FormatNumber(func(x int) (string error) {
+			return fmt.Sprintf("%x", x), nill
+		}),
+		//server.FormatNumber(func(x int) (string, error) { return fmt.Sprintf("%b", x), nil }),  // anonymous fcn
+		//server.FormatNumber(func(x int) (string, error) { return "", errors.New("FormatNumber error") }),  // anonymous fcn
+	)
+
+	if err != nil {
+		Error.Printf("unable to initialize server: %v", err)
+		os.Exit(1)
+	}
+
+	srv := &http.Server{
+		Addr:    ":" + Config.Port,
+		Handler: newServer,
+	}
+
+	go func() {
+		<-quit
+		ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(2*time.Second))
+		defer cancel()
+		Info.Println("shutting down server ... ")
+		if err := srv.Shutdown(ctx); err != nil {
+			Error.Printf("unable to shutdown server: %v", err)
+		}
+	}()
+
+	Error.Println("server started at localhost: " + Config.Port)
+	err = srv.ListenAndServe()
+
+}
